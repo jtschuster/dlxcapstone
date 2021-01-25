0a04f3729dd6f3225d1f217802fa28a6ca509209
jtschuster <jacksontschuster@gmail.com>
2021-01-24 21:27:47 -0600

    r-type instructions controls ready?

--- control.v
+++ control.v
@@ -17,8 +17,26 @@ module control(
    output [4:0] ALUOp;
    output 	Branch;
    output 	MemWr;
-   output 	MemToReg; 
+   output 	MemToReg;
+   
 
+   localparam [4:0]   add_op = 5'h6;
+   localparam [4:0]   sub_op = 5'h6;
+   localparam [4:0]   and_op = 5'h6;
+   localparam [4:0]   or_op  = 5'h6;
+   localparam [4:0]   xor_op = 5'h6;
+   localparam [4:0]   shift_left_op = 5'h6;
+   localparam [4:0]   shift_right_logical_op = 5'h6;
+   localparam [4:0]   shift_right_arithmentic_op = 5'h6;
+   localparam [4:0]   set_eq_op = 5'h6;
+   localparam [4:0]   set_neq_op = 5'h6;
+   localparam [4:0]   set_gt_op = 5'h6;
+   localparam [4:0]   set_lt_op = 5'h6;
+   localparam [4:0]   set_geq_op = 5'h6;
+   localparam [4:0]   set_leq_op = 5'h6;
+   
+
+   
    wire 	r_type;
    wire [5:0] 	opcode;
    wire [5:0] 	rd;
@@ -36,7 +54,7 @@ module control(
 
    wire 	branch_instr;
    
-
+   
    assign nop_func = 6'h15;
 
 
@@ -46,32 +64,37 @@ module control(
    assign rs1 = instr[15:11];
    assign rs2 = instr[20:16];
    assign func = instr[31:27];
-
+   
    
    assign r_type = !(| opcode) && (~ func == nop_func) | 
-		   (opcode == 6'h01);
-   assign j_type = opcode == 6'h02 | // J
-		   opcode == 6'h03 | // JAL
-		   opcode == 6'h10 | // RFE
-		   opcode == 6'h11;  // TRAP
-   assign branch_instr = opcode == 6'h04 | // BEQZ
-			 opcode == 6'h05 | // BNEZ
-			 opcode == 6'h06 | // BFPT
-			 opcode == 6'h07;  // BFPR
+		   (opcode == 6'h01); // MULT, MULTU, and a bunch of FP instructions
+   // Only for the always jumps, doesn't include branches
+   assign j_type[5:0] = opcode == 6'h02 | // J
+			opcode == 6'h03 | // JAL
+			opcode == 6'h10 | // RFE
+			opcode == 6'h11;  // TRAP
+   assign branch_instr[5:0] = opcode == 6'h04 | // BEQZ
+			      opcode == 6'h05 | // BNEZ
+			      opcode == 6'h06 | // BFPT
+			      opcode == 6'h07;  // BFPR
    
    
-   assign RegWr  = r_type |  
-		   i_type & ;
+   assign RegWr  = r_type;
    assign RegDst = rd;
+   // 1 if sign extend, 0 if 0 extend
+   assign ExtOp    = 'b0;
+   // 1 if reg, 0 if immediate
+   assign ALUSrc   = r_type;
    
-   assign ExtOp    = '0';
-   assign ALUSrc   = '0';
-   assign ALUOp    = '0';
+   assign ALUOp    = ;
+   
    assign Branch   = j_type |
 		     i_type && branch_instr;
-   assign MemWr    = '0';
-   assign MemToReg = '0';
-   
+   assign MemWr    = 'b0;
+   assign MemToReg = 'b0;
+
+
+    
 
 endmodule
 	
